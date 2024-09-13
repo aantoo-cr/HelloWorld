d36f4ca65331c300b8a977eef249cc50937832a5
Antonia Córdova <a.cordova02@ufromail.cl>
2024-09-13 14:08:33 -0300

    He agregado los cambios útiles (nombres) realizados desde las otras ramas para así hacer un único merge al main con todo lo trabajado.

--- src/main/java/HolaMundo.java
+++ src/main/java/HolaMundo.java
@@ -2,5 +2,7 @@ public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("Hola Mundo!!!/");
         System.out.println("Antonia Córdova");
+        System.out.println("Alex Rojas");
+        System.out.println("Antonio Acuña");
     }
     }
